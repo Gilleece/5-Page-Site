cc21d6d5b158e82c3ab4cd165863fd6ab974a495
Sean Gilleece <seangilleece@gmail.com>
2021-11-12 11:01:15 +0000

    Site finished

---
+++ JS/events.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    var event1 = document.getElementById("event1");
+    event1.onclick = function() {
+        event1.style.color = "red";
+        event1.textContent="More details (visited)";
+    }
+    var event2 = document.getElementById("event2");
+    event2.onclick = function() {
+        event2.style.color = "red";
+        event2.textContent="More details (visited)";
+    }
+    var event3 = document.getElementById("event3");
+    event3.onclick = function() {
+        event3.style.color = "red";
+        event3.textContent="More details (visited)";
+    }
+    var event4 = document.getElementById("event4");
+    event4.onclick = function() {
+        event4.style.color = "red";
+        event4.textContent="More details (visited)";
+    }
+    var event5 = document.getElementById("event5");
+    event5.onclick = function() {
+        event5.style.color = "red";
+        event5.textContent="More details (visited)";
+    }
+    var event6 = document.getElementById("event6");
+    event6.onclick = function() {
+        event6.style.color = "red";
+        event6.textContent="More details (visited)";
+    }
+  }
